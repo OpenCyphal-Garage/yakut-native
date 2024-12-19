3776ce4b9c83cf79b3dd581c541ae32abfe48e36
Sergei Shirokov <sergei.shirokov@zubax.com>
2024-12-19 16:05:34 +0200

    added kqueue-based BSD executor

--- src/daemon/engine/platform/defines.hpp
+++ src/daemon/engine/platform/defines.hpp
@@ -6,7 +6,11 @@
 #ifndef OCVSMD_DAEMON_ENGINE_PLATFORM_DEFINES_HPP_INCLUDED
 #define OCVSMD_DAEMON_ENGINE_PLATFORM_DEFINES_HPP_INCLUDED
 
+#ifdef PLATFORM_LINUX_TYPE_BSD
+#include "bsd/kqueue_single_threaded_executor.hpp"
+#else
 #include "debian/epoll_single_threaded_executor.hpp"
+#endif
 
 namespace ocvsmd
 {
@@ -17,7 +21,11 @@ namespace engine
 namespace platform
 {
 
+#ifdef PLATFORM_LINUX_TYPE_BSD
+using SingleThreadedExecutor = bsd::KqueueSingleThreadedExecutor;
+#else
 using SingleThreadedExecutor = debian::EpollSingleThreadedExecutor;
+#endif
 
 }  // namespace platform
 }  // namespace engine
