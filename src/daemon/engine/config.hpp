25eb5b06f6a76c52d5c2e6a836687e2b14a795d3
Sergei Shirokov <sergei.shirokov@zubax.com>
2025-02-07 16:07:54 +0200

    more copy as is from `sshirokov/file_server` branch

--- src/daemon/engine/config.hpp
+++ src/daemon/engine/config.hpp
@@ -6,6 +6,15 @@
 #ifndef OCVSMD_DAEMON_ENGINE_CONFIG_HPP_INCLUDED
 #define OCVSMD_DAEMON_ENGINE_CONFIG_HPP_INCLUDED
 
+#include <cetl/cetl.hpp>
+#include <cetl/pf17/cetlpf.hpp>
+
+#include <array>
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <vector>
+
 namespace ocvsmd
 {
 namespace daemon
@@ -14,7 +23,46 @@ namespace engine
 {
 
 class Config
-{};  // Config
+{
+public:
+    using Ptr = std::shared_ptr<Config>;
+
+    struct CyphalApp
+    {
+        using NodeId   = std::uint16_t;
+        using UniqueId = std::array<std::uint8_t, 16>;  // NOLINT(*-magic-numbers)
+    };
+
+    CETL_NODISCARD static Ptr make(std::string file_path);
+
+    Config(const Config&)                = delete;
+    Config(Config&&) noexcept            = delete;
+    Config& operator=(const Config&)     = delete;
+    Config& operator=(Config&&) noexcept = delete;
+
+    virtual ~Config() = default;
+
+    virtual void save() = 0;
+
+    CETL_NODISCARD virtual auto getCyphalAppNodeId() const -> cetl::optional<CyphalApp::NodeId>     = 0;
+    CETL_NODISCARD virtual auto getCyphalAppUniqueId() const -> cetl::optional<CyphalApp::UniqueId> = 0;
+    virtual void                setCyphalAppUniqueId(const CyphalApp::UniqueId& unique_id)          = 0;
+
+    CETL_NODISCARD virtual auto getCyphalTransportInterfaces() const -> std::vector<std::string> = 0;
+
+    CETL_NODISCARD virtual auto getFileServerRoots() const -> std::vector<std::string>    = 0;
+    virtual void                setFileServerRoots(const std::vector<std::string>& roots) = 0;
+
+    CETL_NODISCARD virtual auto getIpcConnections() const -> std::vector<std::string> = 0;
+
+    CETL_NODISCARD virtual auto getLoggingFile() const -> cetl::optional<std::string>       = 0;
+    CETL_NODISCARD virtual auto getLoggingLevel() const -> cetl::optional<std::string>      = 0;
+    CETL_NODISCARD virtual auto getLoggingFlushLevel() const -> cetl::optional<std::string> = 0;
+
+protected:
+    Config() = default;
+
+};  // Config
 
 }  // namespace engine
 }  // namespace daemon
