1bf7f665c7ef0b890c89d3e4e0eff5c9bbbb3dd9
Sergei Shirokov <sergei.shirokov@zubax.com>
2025-02-07 16:19:11 +0200

    more copy as is from `sshirokov/file_server` branch

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -3,17 +3,124 @@
 // SPDX-License-Identifier: MIT
 //
 
+#include "setup_logging.hpp"
+
+#include <ocvsmd/platform/defines.hpp>
 #include <ocvsmd/sdk/daemon.hpp>
+#include <ocvsmd/sdk/execution.hpp>
+#include <ocvsmd/sdk/node_command_client.hpp>
+
+#include <cetl/pf17/cetlpf.hpp>
+
+#include <spdlog/spdlog.h>
+
+#include <cerrno>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <exception>
+#include <iostream>
+#include <signal.h>  // NOLINT
+#include <utility>
+#include <vector>
+
+namespace
+{
+
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+volatile sig_atomic_t g_running = 1;
+
+void signalHandler(const int sig)
+{
+    switch (sig)
+    {
+    case SIGINT:
+    case SIGTERM:
+        g_running = 0;
+        break;
+    default:
+        break;
+    }
+}
+
+void setupSignalHandlers()
+{
+    struct sigaction sigbreak
+    {};
+    sigbreak.sa_handler = &signalHandler;
+    ::sigaction(SIGINT, &sigbreak, nullptr);
+    ::sigaction(SIGTERM, &sigbreak, nullptr);
+}
+
+}  // namespace
 
 int main(const int argc, const char** const argv)
 {
-    (void) argc;
-    (void) argv;
+    using std::chrono_literals::operator""s;
+    using Executor = ocvsmd::platform::SingleThreadedExecutor;
 
-    if (auto daemon = ocvsmd::sdk::Daemon::make())
+    setupSignalHandlers();
+    setupLogging(argc, argv);
+
+    spdlog::info("OCVSMD client started (ver='{}.{}').", VERSION_MAJOR, VERSION_MINOR);
+    int result = EXIT_SUCCESS;
+    try
     {
-        daemon->send_messages();
-    }
+        auto&    memory = *cetl::pmr::new_delete_resource();
+        Executor executor;
 
-    return 0;
+        std::string ipc_connection = "unix-abstract:org.opencyphal.ocvsmd.ipc";
+        if (const auto* const env_connection_str = std::getenv("OCVSMD_CONNECTION"))
+        {
+            ipc_connection = env_connection_str;
+        }
+
+        const auto daemon = ocvsmd::sdk::Daemon::make(memory, executor, ipc_connection);
+        if (!daemon)
+        {
+            spdlog::critical("Failed to create daemon.");
+            std::cerr << "Failed to create daemon.";
+            return EXIT_FAILURE;
+        }
+
+        // Demo of daemon's node command client, sending a command to node 42, 43 & 44.
+        {
+            using Command = ocvsmd::sdk::NodeCommandClient::Command;
+
+            auto node_cmd_client = daemon->getNodeCommandClient();
+
+            const std::vector<std::uint16_t> node_ids = {42};
+            // auto sender     = node_cmd_client->restart({node_ids.data(), node_ids.size()});
+            auto sender     = node_cmd_client->beginSoftwareUpdate({node_ids.data(), node_ids.size()}, "firmware.bin");
+            auto cmd_result = ocvsmd::sdk::sync_wait<Command::Result>(executor, std::move(sender));
+
+            if (const auto* const err = cetl::get_if<Command::Failure>(&cmd_result))
+            {
+                spdlog::error("Failed to send command: {}", std::strerror(*err));
+            }
+            else
+            {
+                const auto responds = cetl::get<Command::Success>(std::move(cmd_result));
+                for (const auto& node_and_respond : responds)
+                {
+                    spdlog::info("Node {} responded with status: {}.",
+                                 node_and_respond.first,
+                                 node_and_respond.second.status);
+                }
+            }
+        }
+
+        if (g_running == 0)
+        {
+            spdlog::debug("Received termination signal.");
+        }
+
+    } catch (const std::exception& ex)
+    {
+        spdlog::critical("Unhandled exception: {}", ex.what());
+        result = EXIT_FAILURE;
+    }
+    spdlog::info("OCVSMD client terminated.");
+
+    return result;
 }
--- src/daemon/engine/config.cpp
+++ src/daemon/engine/config.cpp
@@ -2,3 +2,156 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT
 //
+
+#include "config.hpp"
+
+#include <cetl/pf17/cetlpf.hpp>
+
+#include <spdlog/spdlog.h>
+#include <toml.hpp>
+
+#include <chrono>
+#include <exception>
+#include <fstream>
+#include <ios>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace ocvsmd
+{
+namespace daemon
+{
+namespace engine
+{
+namespace
+{
+
+class ConfigImpl final : public Config
+{
+public:
+    using TomlConf  = toml::ordered_type_config;
+    using TomlValue = toml::basic_value<TomlConf>;
+
+    ConfigImpl(std::string file_path, TomlValue&& root)
+        : file_path_{std::move(file_path)}
+        , root_{std::move(root)}
+        , is_dirty_{false}
+    {
+    }
+
+    // Config
+
+    void save() override
+    {
+        if (is_dirty_)
+        {
+            try
+            {
+                root_["__meta__"]["last_modified"] = std::chrono::system_clock::now();
+
+                const auto    cfg_str = format(root_);
+                std::ofstream file{file_path_, std::ios_base::out | std::ios_base::binary};
+                file << cfg_str;
+
+                is_dirty_ = false;
+
+            } catch (const std::exception& ex)
+            {
+                spdlog::error("Failed to save config. Error: {}", file_path_, ex.what());
+            }
+        }
+    }
+
+    auto getCyphalAppNodeId() const -> cetl::optional<CyphalApp::NodeId> override
+    {
+        return findImpl<CyphalApp::NodeId>("cyphal", "application", "node_id");
+    }
+
+    auto getCyphalAppUniqueId() const -> cetl::optional<CyphalApp::UniqueId> override
+    {
+        return findImpl<CyphalApp::UniqueId>("cyphal", "application", "unique_id");
+    }
+
+    void setCyphalAppUniqueId(const CyphalApp::UniqueId& unique_id) override
+    {
+        auto& toml_unique_id = root_["cyphal"]["application"]["unique_id"];
+        toml_unique_id       = unique_id;
+        for (auto& item : toml_unique_id.as_array())
+        {
+            item.as_integer_fmt().fmt = toml::integer_format::hex;
+        }
+        toml_unique_id.as_array_fmt().fmt = toml::array_format::oneline;
+
+        is_dirty_ = true;
+    }
+
+    auto getCyphalTransportInterfaces() const -> std::vector<std::string> override
+    {
+        return find_or(root_, "cyphal", "transport", "interfaces", std::vector<std::string>{});
+    }
+
+    auto getFileServerRoots() const -> std::vector<std::string> override
+    {
+        return find_or(root_, "file_server", "roots", std::vector<std::string>{});
+    }
+
+    void setFileServerRoots(const std::vector<std::string>& roots) override
+    {
+        auto& toml_fs_roots = root_["file_server"]["roots"];
+        toml_fs_roots       = roots;
+        is_dirty_           = true;
+    }
+
+    auto getIpcConnections() const -> std::vector<std::string> override
+    {
+        return find_or(root_, "ipc", "connections", std::vector<std::string>{});
+    }
+
+    auto getLoggingFile() const -> cetl::optional<std::string> override
+    {
+        return findImpl<std::string>("logging", "file");
+    }
+
+    auto getLoggingLevel() const -> cetl::optional<std::string> override
+    {
+        return findImpl<std::string>("logging", "level");
+    }
+
+    auto getLoggingFlushLevel() const -> cetl::optional<std::string> override
+    {
+        return findImpl<std::string>("logging", "flush_level");
+    }
+
+private:
+    template <typename T, typename... Keys>
+    cetl::optional<T> findImpl(Keys&&... keys) const
+    {
+        try
+        {
+            return cetl::make_optional(toml::find<T>(root_, std::forward<Keys>(keys)...));
+
+        } catch (...)
+        {
+            return cetl::nullopt;
+        }
+    }
+
+    std::string file_path_;
+    TomlValue   root_;
+    bool        is_dirty_;
+
+};  // ConfigImpl
+
+}  // namespace
+
+Config::Ptr Config::make(std::string file_path)
+{
+    auto root = toml::parse<ConfigImpl::TomlConf>(file_path);
+    return std::make_shared<ConfigImpl>(std::move(file_path), std::move(root));
+}
+
+}  // namespace engine
+}  // namespace daemon
+}  // namespace ocvsmd
